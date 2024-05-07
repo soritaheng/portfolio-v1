ae0e708b25dde5f57d31765a6224fcf426974068
soritahg <69398037+soritaheng@users.noreply.github.com>
2024-05-08 00:26:15 +1000

    set up nav links

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +17,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${inter.className} flex`}>
+        <header className="flex flex-col">
+          <Link href="/">Home</Link>
+          <Link href="/projects">Projects</Link>
+          <Link href="/contact">Contact</Link>
+        </header>
+        {children}
+        </body>
     </html>
   );
 }
