37842f6bc90928646f99c738c6da4797e4b01e75
soritahg <69398037+soritaheng@users.noreply.github.com>
2024-05-10 15:26:21 +1000

    add ibm plex mono font

---
+++ src/app/fonts.ts
@@ -0,0 +1,12 @@
+import { Inter, IBM_Plex_Mono } from 'next/font/google'
+ 
+export const inter = Inter({
+  subsets: ['latin'],
+  display: 'swap',
+})
+ 
+export const ibm_plex_mono = IBM_Plex_Mono({
+  subsets: ['latin'],
+  weight: '400',
+  display: 'swap',
+})
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,9 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { inter, ibm_plex_mono } from "./fonts";
 import "./globals.css";
 import Link from "next/link";
 
-const inter = Inter({ subsets: ["latin"] });
-const links = [{path: '/', name: 'Home'}, { path: '/projects', name: 'Projects'}, { path: '/contact', name: 'Contact'}];
+const pages = [{path: '/', name: 'Home'}, { path: '/projects', name: 'Projects'}, { path: '/contact', name: 'Contact'}];
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -18,10 +17,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${inter.className} flex`}>
-        <header className="flex flex-col gap-4 font-semibold text-4xl">
-          {links.map((link) => (
-            <Link className="hover:underline-offset-8 hover:underline" href={link.path}>{link.name}</Link>
+      <body className={`${ibm_plex_mono.className} flex m-8`}>
+        <header className={`${inter.className} flex flex-col gap-4 font-semibold text-4xl`}>
+          {pages.map((page) => (
+            <Link className="hover:underline-offset-8 hover:underline" key={page.name} href={page.path}>{page.name}</Link>
           ))}
         </header>
         {children}
