301b67d5a9e1c6107a626993e803f04862be482f
soritahg <69398037+soritaheng@users.noreply.github.com>
2024-05-10 16:50:06 +1000

    add page content

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,5 +1,7 @@
 export default function Contact() {
     return(
-        <div>If you think we'll be a good fit for a project, feel free to reach out to me.</div>
+        <div className="max-w-[65ch]">
+            <p>If you think we'll be a good fit for a project, feel free to reach out to me.</p>
+        </div>
     )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,17 @@
-import Link from "next/link";
-
 export default function Home() {
   return (
       <div className="max-w-[65ch]">
-        <p>Hi there, I’m Sorita. Thanks for stopping by my small corner of the internet! Here you'll find projects I took part in building during my time at Coolbeans Digital, as well as mini personal projects I built when I first started learning web development.</p>
-        <p></p>
+        <p className="mb-12">Hi there, I’m Sorita. Thanks for stopping by my small corner of the internet! Here you'll find projects I took part in building during my time at Coolbeans Digital, as well as mini personal projects I made when I first started learning web development.</p>
+        <div className="space-y-6">
+          <div>
+            <p className="italic mb-1">What I'm up to now:</p>
+            <p className="text-sm">Pursuing my master's degree in Information Technology at Queensland University of Technology (QUT) in Brisbane.</p>
+          </div>
+          <div>
+            <p className="italic mb-1">What I'm excited about:</p>
+            <p className="text-sm">Recent advances in AI and their potential applications!</p>
+          </div>
+        </div>
       </div>
   );
 }
