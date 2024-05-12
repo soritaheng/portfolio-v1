fbf783ed32b95b47d24c7aa70f32e4de2b62961f
soritahg <69398037+soritaheng@users.noreply.github.com>
2024-05-12 14:51:06 +1000

    add responsive text styling

--- src/app/projects/project.tsx
+++ src/app/projects/project.tsx
@@ -3,7 +3,7 @@ import type { ProjectType } from "../types"
 export default function Project({ name, url, role, duration, description }: ProjectType) {
     return(
         <div>
-            <h1 className="font-semibold text-xl leading-relaxed">{name}</h1>
+            <h1 className="font-semibold md:text-xl leading-relaxed">{name}</h1>
             <a href={url} target="_blank" className="italic underline text-sm leading-relaxed">View live site</a>
             <div className="flex gap-8 text-sm leading-relaxed mb-2">
                 <p><strong>Role: </strong>{role}</p>
